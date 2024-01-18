11c0330a61dd8a062664a76e11e0534c14783149
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2024-01-17 22:16:50 -0300

    feat: add hamburger button

--- src/components/Navbar/Index.tsx
+++ src/components/Navbar/Index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import AdbIcon from '@mui/icons-material/Adb';
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import { useState } from 'react';
+import './index.css';
+
+//import logo from '../../assets/logo.png';
 
 function NavBar() {
+  const [menu, setMenu] = useState(false);
+
   return (
     <AppBar position="static" color="primary">
       <Grid
@@ -18,20 +21,79 @@ function NavBar() {
         padding="0.80rem"
       >
         <Grid item display="flex" marginLeft="6rem">
-          <AdbIcon sx={{ marginRight: '1.5rem' }} />
+          <Box
+            component="img"
+            src=""
+            alt="Logo"
+            sx={{
+              objectFit: 'cover',
+            }}
+          />
           <Typography variant="h5">V2 Engenharia</Typography>
         </Grid>
-        <Grid item marginRight="6rem">
-          <Button variant="text" sx={{ color: '#ffff' }}>
+        <Grid item marginRight="6rem" className="menuDesk">
+          <Button variant="text" sx={{ color: '#24274C' }}>
             Sobre nós
           </Button>
-          <Button variant="text" sx={{ color: '#ffff' }}>
+          <Button variant="text" sx={{ color: '#24274C' }}>
             Serviços
           </Button>
-          <Button variant="text" sx={{ color: '#ffff' }}>
+          <Button variant="text" sx={{ color: '#24274C' }}>
             Projetos
           </Button>
           <Button variant="contained" color="secondary">
+            Entre em contato
+          </Button>
+        </Grid>
+
+        {/* HAMBURGER BUTTON */}
+
+        <img
+          alt="Menu"
+          src=""
+          className="imgMob"
+          onClick={() => setMenu(!menu)}
+        />
+
+        <Grid
+          item
+          className="menuMob"
+          sx={{
+            display: menu ? 'flex' : 'none',
+            position: 'absolute',
+            top: '3.5rem',
+            right: '0rem',
+            zIndex: '2rem',
+            flexDirection: 'column',
+            backgroundColor: '#7eb5c6',
+          }}
+        >
+          <Button
+            variant="text"
+            sx={{ color: '#24274C' }}
+            onClick={() => setMenu(false)}
+          >
+            Sobre nós
+          </Button>
+          <Button
+            variant="text"
+            sx={{ color: '#24274C' }}
+            onClick={() => setMenu(false)}
+          >
+            Serviços
+          </Button>
+          <Button
+            variant="text"
+            sx={{ color: '#24274C' }}
+            onClick={() => setMenu(false)}
+          >
+            Projetos
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => setMenu(false)}
+          >
             Entre em contato
           </Button>
         </Grid>
--- src/config/theme/defaultTheme.ts
+++ src/config/theme/defaultTheme.ts
@@ -1,14 +1,16 @@
 import { createTheme } from '@mui/material/styles';
 
+//LOGO: #7EB5C6 E #24274C
+
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: '#0064e0',
-      contrastText: '#ffff',
+      main: '#7EB5C6',
+      contrastText: '#24274C',
     },
     secondary: {
-      main: '#ffff',
-      contrastText: '#0064e0',
+      main: '#24274C',
+      contrastText: '#FFFF',
     },
   },
   typography: {
